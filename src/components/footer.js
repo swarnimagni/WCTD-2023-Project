da5451f0337fa79d9285670c71259ecc1e9c41df
VentureCharms <68164728+VentureCharms@users.noreply.github.com>
2023-01-28 12:56:45 -0600

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,9 @@
 export default function Footer() {
     return <div className="footer">
         <div className="socials">
-            <a className="socials-link" href="https://www.instagram.com/tech_interrogation"><img className="socials-image" style={{height: "80%", position: "relative", bottom: "10%"}} src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/640px-Instagram_icon.png" alt="Instagram"></img></a>
-            <a className="socials-link" href="https://twitter.com/tech_interrogat"><img className="socials-image" src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c53e.png" alt="Twitter"></img></a>
-            <a className="socials-link" href="https://www.youtube.com/channel/UCtkS3dHNbFxk1QD5F3yWnhg"><img className="socials-image" src="https://www.iconpacks.net/icons/2/free-youtube-logo-icon-2431-thumb.png" alt="YouTube"></img></a>
+            <a className="socials-link" target="_blank" rel="noreferrer" href="https://www.instagram.com/tech_interrogation"><img className="socials-image" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/640px-Instagram_icon.png" alt="Instagram"></img></a>
+            <a className="socials-link" target="_blank" rel="noreferrer" href="https://twitter.com/tech_interrogat"><img className="socials-image" src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c53e.png" alt="Twitter"></img></a>
+            <a className="socials-link" target="_blank" rel="noreferrer" href="https://www.youtube.com/channel/UCtkS3dHNbFxk1QD5F3yWnhg"><img className="socials-image" src="https://www.iconpacks.net/icons/2/free-youtube-logo-icon-2431-thumb.png" alt="YouTube"></img></a>
         </div>
     </div>;
 }
