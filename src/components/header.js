14e9943fa5a8d96eae6c9ff7e97b144b42726cc4
VentureCharms <68164728+VentureCharms@users.noreply.github.com>
2023-01-28 14:13:01 -0600

    Commit for About Us

--- src/components/header.js
+++ src/components/header.js
@@ -1,3 +1,15 @@
+import logo from "../logo.png";
+
 export default function Header() {
-    return <div className="header">Tech Interrogation</div>
+    return <div className="header">
+                <a href="/">
+                    <img src={logo} alt="Logo" className="header-item header-logo"></img>
+                </a>
+                <a href="/">
+                    <div className="header-item nav-button">Home</div>
+                </a>
+                <a href="/demonstration">
+                    <div className="header-item nav-button">Demonstration</div>
+                </a>
+            </div>
 }
