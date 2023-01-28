aa7f5915c2b3246cc0b176eef7f7a94cd21c0a3f
VentureCharms <68164728+VentureCharms@users.noreply.github.com>
2023-01-28 15:19:49 -0600

    Upgraded Footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,9 @@
 export default function Footer() {
     return <div className="footer">
         <div className="socials">
-            <a className="socials-link" target="_blank" rel="noreferrer" href="https://www.instagram.com/tech_interrogation"><img className="socials-image" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/640px-Instagram_icon.png" alt="Instagram"></img></a>
-            <a className="socials-link" target="_blank" rel="noreferrer" href="https://twitter.com/tech_interrogat"><img className="socials-image" src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c53e.png" alt="Twitter"></img></a>
-            <a className="socials-link" target="_blank" rel="noreferrer" href="https://www.youtube.com/channel/UCtkS3dHNbFxk1QD5F3yWnhg"><img className="socials-image" src="https://www.iconpacks.net/icons/2/free-youtube-logo-icon-2431-thumb.png" alt="YouTube"></img></a>
+            <a target="_blank" rel="noreferrer" href="https://www.instagram.com/tech_interrogation"><img className="footer-item" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/640px-Instagram_icon.png" alt="Instagram"></img></a>
+            <a target="_blank" rel="noreferrer" href="https://twitter.com/tech_interrogat"><img className="footer-item" src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c53e.png" alt="Twitter"></img></a>
+            <a target="_blank" rel="noreferrer" href="https://www.youtube.com/channel/UCtkS3dHNbFxk1QD5F3yWnhg"><img className="footer-item" src="https://www.iconpacks.net/icons/2/free-youtube-logo-icon-2431-thumb.png" alt="YouTube"></img></a>
         </div>
     </div>;
 }
--- src/views/aboutUs/components/aboutUs.js
+++ src/views/aboutUs/components/aboutUs.js
@@ -1,18 +1,18 @@
 export default function AboutUs(){   
     return <>
-        <div class="pics">
+        <div className="pics">
         <img src="https://i.pinimg.com/736x/aa/8c/20/aa8c201df553f6e0552f05a4a3e70b2f--pixel-cat-kawaii-cat.jpg" alt="Place Holder" width="300" height="300"></img>
-        <div class="desc">desc</div>
+        <div className="desc">desc</div>
         </div>
 
-        <div class="pics">
+        <div className="pics">
         <img src="https://i.pinimg.com/736x/aa/8c/20/aa8c201df553f6e0552f05a4a3e70b2f--pixel-cat-kawaii-cat.jpg" alt="Place Holder" width="300" height="300"></img>
-        <div class="desc">desc</div>
+        <div className="desc">desc</div>
         </div>
 
-        <div class="pics">
+        <div className="pics">
         <img src="https://i.pinimg.com/736x/aa/8c/20/aa8c201df553f6e0552f05a4a3e70b2f--pixel-cat-kawaii-cat.jpg" alt="Place Holder" width="300" height="300"></img>
-        <div class="desc">desc</div>
+        <div className="desc">desc</div>
         
         </div>
     </>
