2666b8ba48a65541bc1322263404dc2939bf6dd5
VentureCharms <68164728+VentureCharms@users.noreply.github.com>
2023-01-28 19:58:47 -0600

    Edited Header

--- src/components/header.js
+++ src/components/header.js
@@ -1,25 +1,18 @@
 import logo from "../logo.png";
 
 export default function Header() {
-return <div className="Header"> <ul>
-    <li>
-    <a href="/">
-                    <img src={logo} alt="Logo" className="header-item header-logo"></img>
-                </a>
-    <a href="/">
-        <div className="header-item nav-button">Home</div>
-    </a>
-  </li>
-  <li>
-    <a href="/demonstration">
-        <div className="header-item nav-button">Demonstration</div>
-    </a>
-  </li>
-  <li>
-    <a href="/aboutUs">
-       <div className="header-item nav-button">About The Creators</div>
-    </a>
-  </li>
-</ul>
+return <div className="header">
+      <a href="/">
+          <img src={logo} alt="Logo" className="header-item"></img>
+      </a>
+      <a href="/">
+          <div className="header-item nav-button">Home</div>
+      </a>
+      <a href="/demonstration">
+          <div className="header-item nav-button">Demonstration</div>
+      </a>
+      <a href="/aboutUs">
+        <div className="header-item nav-button">About The Creators</div>
+      </a>
 </div>
 }
