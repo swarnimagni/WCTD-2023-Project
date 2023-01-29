9fbcaab89a3afa3e8fd5d7c905311a56d8c488dd
VentureCharms <68164728+VentureCharms@users.noreply.github.com>
2023-01-29 09:56:17 -0600

    Cleaned Up Website

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,7 @@ return <div className="header">
       <a href="/">
           <img src={logo} alt="Logo" className="header-item logo"></img>
       </a>
-      <div class="header-text">
+      <div className="header-text">
       Tech Interrogation
       </div>
       <HamburgerMenu/>
