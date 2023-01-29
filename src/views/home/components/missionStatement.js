e553cccab983a8ed545d8f5c280d16b520dfc065
VentureCharms <68164728+VentureCharms@users.noreply.github.com>
2023-01-28 19:15:14 -0600

    Updated Mission Statement

--- src/views/home/components/missionStatement.js
+++ src/views/home/components/missionStatement.js
@@ -1,6 +1,3 @@
 export default function MissionStatement(){
-    return <div className="mission-statement">
-            <div className="mission-statement-title">Our Mission Statement:</div>
-            <div className="mission-statement-content">Our goal at Tech Interrogation is to deliver effective services that make our criminal justice heroes' lives easier. We prioritize honesty, integrity, and the truth. Using our revolutionary facial recognition technology, we strive to embed these principles into our society, one interrogation at a time.</div>
-        </div>
+    return <div className="mission-statement-content">Our goal at Tech Interrogation is to deliver effective services that make our criminal justice heroes' lives easier. We prioritize honesty, integrity, and the truth. Using our revolutionary facial recognition technology, we strive to embed these principles into our society, one interrogation at a time.</div>
 }
