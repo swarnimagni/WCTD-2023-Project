beb3941c9892a7bf1d633b18807d4b472fce3d9e
VentureCharms <68164728+VentureCharms@users.noreply.github.com>
2023-01-29 10:25:30 -0600

    Fixed Contact Us

--- src/views/contactUs/components/contactUs.js
+++ src/views/contactUs/components/contactUs.js
@@ -1,10 +1,13 @@
 export default function ContactUs() {
     return (
-            <><h1>Contact Us</h1><form>
-            <input type="text" placeholder="Name" />
-            <input type="email" placeholder="Email" />
-            <textarea placeholder="Message"></textarea>
-            <button type="submit">Submit</button>
-        </form></>
+        <>
+            <h1>Contact Us</h1>
+            <form>
+                <input type="text" placeholder="Name" />
+                <input type="email" placeholder="Email" />
+                <textarea placeholder="Message"></textarea>
+                <button type="submit">Submit</button>
+            </form>
+        </>
     );
   }
