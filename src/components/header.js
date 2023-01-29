3f12be4c23dba91ef5b0263bda1d4cb06ca699a8
VentureCharms <68164728+VentureCharms@users.noreply.github.com>
2023-01-28 21:21:03 -0600

    Added Testimonial Buttons

--- src/components/header.js
+++ src/components/header.js
@@ -3,19 +3,21 @@ import logo from "../logo.png";
 export default function Header() {
 return <div className="header">
       <a href="/">
-          <img src={logo} alt="Logo" className="header-item"></img>
+          <img src={logo} alt="Logo" className="header-item logo"></img>
       </a>
-      <a href="/">
-          <div className="header-item nav-button">Home</div>
-      </a>
-      <a href="/demonstration">
-          <div className="header-item nav-button">Demonstration</div>
-      </a>
-      <a href="/aboutUs">
-        <div className="header-item nav-button">About The Creators</div>
-      </a>
-      <a href="/testimonials">
-        <div className="header-item nav-button">Testimonials</div>
-      </a>
+      <div className="nav-buttons">
+        <a href="/">
+            <div className="header-item nav-button">Home</div>
+        </a>
+        <a href="/demonstration">
+            <div className="header-item nav-button">Demonstration</div>
+        </a>
+        <a href="/aboutUs">
+          <div className="header-item nav-button">About The Creators</div>
+        </a>
+        <a href="/testimonials">
+          <div className="header-item nav-button">Testimonials</div>
+        </a>
+      </div>
 </div>
 }
--- src/views/testimonials/components/testimonial.js
+++
@@ -1,6 +0,0 @@
-export default function Testimonial(element){   
-    return <div className="testimonial">
-            <div className="testimonial-name">{element.name}</div>
-            <div className="testimonial-desc">{element.testimonial}</div>
-        </div>
-}
