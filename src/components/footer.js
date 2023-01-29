b3ac1780742ecdc527a4fccacfff2f055150918d
VentureCharms <68164728+VentureCharms@users.noreply.github.com>
2023-01-29 08:51:50 -0600

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
     return <div className="footer">
         <div className="socials">
             <a target="_blank" rel="noreferrer" href="https://www.instagram.com/tech_interrogation"><img className="footer-item" src={instagram} alt="Instagram"></img></a>
-            <a target="_blank" rel="noreferrer" href="https://twitter.com/tech_interrogat"><img className="footer-item" src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c53e.png" alt="Twitter"></img></a>
+            <a target="_blank" rel="noreferrer" href="https://twitter.com/tech_interrogat"><img className="footer-item" src="https://www.freepnglogos.com/uploads/twitter-logo-png/twitter-logo-vector-png-clipart-1.png" alt="Twitter"></img></a>
             <a target="_blank" rel="noreferrer" href="https://www.youtube.com/channel/UCtkS3dHNbFxk1QD5F3yWnhg"><img className="footer-item" src="https://www.iconpacks.net/icons/2/free-youtube-logo-icon-2431-thumb.png" alt="YouTube"></img></a>
         </div>
     </div>;
