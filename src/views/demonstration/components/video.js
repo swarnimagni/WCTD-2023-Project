8d2fd9cac558194499c76dbe2e3e58154ce27b16
swarnimagni <swarnim.agnihotri@gmail.com>
2023-01-28 21:27:44 -0500

    Added link to machine

--- src/views/demonstration/components/video.js
+++ src/views/demonstration/components/video.js
@@ -4,5 +4,6 @@ export default function Header (){
     return <div className="demonstration">
         <div className="demonstration-description">Below is a video that we created to demonstrate the capabilities of our app.</div>
         <video controls className="demonstration-video" src={demonstrationVideo}>AI_PRODUCT_NAME</video>
+        <div className="demonstration-description">You can try the tool yourself: <a href="https://teachablemachine.withgoogle.com/models/iny6RXj-N/">HERE</a></div>
     </div>
 }
