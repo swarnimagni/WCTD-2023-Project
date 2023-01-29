46ba2addd3dad8cae90d5da6bf10124d9d6320c9
VentureCharms <68164728+VentureCharms@users.noreply.github.com>
2023-01-29 10:21:20 -0600

    Fixed Demonstration

--- src/views/demonstration/components/video.js
+++ src/views/demonstration/components/video.js
@@ -3,7 +3,7 @@ import demonstrationVideo from "../demonstration-video.mp4";
 export default function Header (){
     return <div className="demonstration">
         <div className="demonstration-description">Below is a video that we created to demonstrate the capabilities of our app.</div>
-        <video controls className="demonstration-video" src={demonstrationVideo}>AI_PRODUCT_NAME</video>
-        <div className="demonstration-description">You can try the tool yourself: <a href="https://teachablemachine.withgoogle.com/models/iny6RXj-N/">HERE</a></div>
+        <video controls className="demonstration-video" src={demonstrationVideo}></video>
+        <div className="demonstration-description">You can try the tool yourself <a href="https://teachablemachine.withgoogle.com/models/iny6RXj-N/">here</a></div>
     </div>
 }
