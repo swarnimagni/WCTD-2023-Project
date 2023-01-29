9d9e4f30d5eeacab4b9c13cbac02679eb3ff8892
VentureCharms <68164728+VentureCharms@users.noreply.github.com>
2023-01-29 10:18:47 -0600

    Fixed Contact Us

--- src/views/contactUs/components/contactUs.js
+++ src/views/contactUs/components/contactUs.js
@@ -10,13 +10,13 @@ export default function ContactUs() {
         </form><div className="social-container">
                 <h3>Follow us on:</h3>
                 <a href="https://www.youtube.com/channel/UCtkS3dHNbFxk1QD5F3yWnhg">
-                    <img src="https://www.iconpacks.net/icons/2/free-youtube-logo-icon-2431-thumb.png" class='small' alt="YouTube"></img>
+                    <img src="https://www.iconpacks.net/icons/2/free-youtube-logo-icon-2431-thumb.png" className='small' alt="YouTube"></img>
                 </a>
                 <a href="https://twitter.com/tech_interrogat">
-                    <img class="small" src="https://www.freepnglogos.com/uploads/twitter-logo-png/twitter-logo-vector-png-clipart-1.png" alt="Twitter"></img>
+                    <img className="small" src="https://www.freepnglogos.com/uploads/twitter-logo-png/twitter-logo-vector-png-clipart-1.png" alt="Twitter"></img>
                 </a>
                 <a href="https://www.instagram.com/tech_interrogation">
-                    <img class="small" src={instagram} alt="Instagram"></img>
+                    <img className="small" src={instagram} alt="Instagram"></img>
                 </a>
             </div></>
     );
