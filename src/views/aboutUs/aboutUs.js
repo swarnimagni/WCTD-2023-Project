faa2dbc26bcbdd7db740c4f8f01a1806c33c9f8f
VentureCharms <68164728+VentureCharms@users.noreply.github.com>
2023-01-28 19:45:42 -0600

    Edited About Us

--- src/views/aboutUs/aboutUs.js
+++ src/views/aboutUs/aboutUs.js
@@ -8,9 +8,9 @@ function App() {
     <div className="App">
       <div className='wrapper'>
         <Header></Header>
-        <AboutUs name="Alexandria"></AboutUs>
-        <AboutUs name="Evan"></AboutUs>
-        <AboutUs name="Swarnim"></AboutUs>
+        <AboutUs name="Alexandria" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."></AboutUs>
+        <AboutUs name="Evan" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."></AboutUs>
+        <AboutUs name="Swarnim" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."></AboutUs>
       </div>
       <Footer></Footer>
     </div>
