32c9563f170d9e0b232a0fba390564be6ccd56a1
VentureCharms <68164728+VentureCharms@users.noreply.github.com>
2023-01-28 19:22:39 -0600

    Updated Instagram Image

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,9 @@
+import instagram from "../instagram.png";
+
 export default function Footer() {
     return <div className="footer">
         <div className="socials">
-            <a target="_blank" rel="noreferrer" href="https://www.instagram.com/tech_interrogation"><img className="footer-item" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/640px-Instagram_icon.png" alt="Instagram"></img></a>
+            <a target="_blank" rel="noreferrer" href="https://www.instagram.com/tech_interrogation"><img className="footer-item" src={instagram} alt="Instagram"></img></a>
             <a target="_blank" rel="noreferrer" href="https://twitter.com/tech_interrogat"><img className="footer-item" src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c53e.png" alt="Twitter"></img></a>
             <a target="_blank" rel="noreferrer" href="https://www.youtube.com/channel/UCtkS3dHNbFxk1QD5F3yWnhg"><img className="footer-item" src="https://www.iconpacks.net/icons/2/free-youtube-logo-icon-2431-thumb.png" alt="YouTube"></img></a>
         </div>
